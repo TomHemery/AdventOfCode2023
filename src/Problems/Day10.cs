55973431cd4a1781457d147372a44dade9ab70ec
Thomas Hemery <tomhemery@outlook.com>
2023-12-13 16:59:28 +0000

    Day10, pants

--- src/Problems/Day10.cs
+++ src/Problems/Day10.cs
@@ -1,3 +1,7 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+
 namespace AdventOfCode2023 
 {
     public class Day10: Problem 
@@ -18,7 +22,9 @@ namespace AdventOfCode2023
                     }
                 }
             }
-            map[(startX, startY)] = GetStartNeighbours(startX, startY);
+            ReplaceStartSquare(startX, startY);
+            foreach(string line in puzzleInputLines) Console.WriteLine(line);
+            map[(startX, startY)] = GetPipeNeighbours(startX, startY, puzzleInputLines[startY][startX]);
         }
 
         protected (int, int)[] GetPipeNeighbours(int x, int y, char pipe)
@@ -38,24 +44,6 @@ namespace AdventOfCode2023
                     return [(x + 1, y), (x, y + 1)];
             }
             return [];
-        }
-
-        protected (int, int)[] GetStartNeighbours(int x, int y)
-        {
-            List<(int, int)> result = new List<(int, int)>();
-            if (x - 1 >= 0 && "-FL".Contains(puzzleInputLines[y][x - 1])) {
-                result.Add((x - 1, y));
-            }
-            if (x + 1 < puzzleInputLines[y].Length && "-7J".Contains(puzzleInputLines[y][x + 1])) {
-                result.Add((x + 1, y));
-            }
-            if (y - 1 >= 0 && "|7F".Contains(puzzleInputLines[y - 1][x])) {
-                result.Add((x, y - 1));
-            }
-            if (y + 1 < puzzleInputLines.Length && "|JL".Contains(puzzleInputLines[y + 1][x])) {
-                result.Add((x, y + 1));
-            }
-            return result.ToArray();
         }
 
         public override string Part1()
@@ -79,57 +67,37 @@ namespace AdventOfCode2023
             var prev = start;
             var curr = map[start][0];
 
-            char[,] adjustedMap = new char[puzzleInputLines[0].Length, puzzleInputLines.Length];
-            for (int x = 0; x < adjustedMap.GetLength(0); x++) {
-                for (int y = 0; y < adjustedMap.GetLength(1); y++) {
-                    adjustedMap[x, y] = ' ';
+            char[,] scaledMap = new char[puzzleInputLines[0].Length * 3, puzzleInputLines.Length * 3];
+            for (int x = 0; x < scaledMap.GetLength(0); x++) {
+                for (int y = 0; y < scaledMap.GetLength(1); y++) {
+                    scaledMap[x, y] = ' ';
                 }
             }
 
-            adjustedMap[curr.Item1, curr.Item2] = '■';
-
+            DrawPipe(start.Item1, start.Item2, scaledMap);
             do {
+                DrawPipe(curr.Item1, curr.Item2, scaledMap);
                 var next = map[curr][0] != prev ? map[curr][0] : map[curr][1];
                 prev = curr;
                 curr = next;
-                adjustedMap[curr.Item1, curr.Item2] = '■';
             } while (curr != start);
 
-            PrintMap(adjustedMap);
-
-            char[,] filledMap = (char[,])adjustedMap.Clone();
-            
-            for (int y = 0; y < adjustedMap.GetLength(1); y++) {
-                for (int x = 0; x < adjustedMap.GetLength(0); x++) {
-                    if (filledMap[x, y] == ' ') {
-                        FloodFill(x, y, filledMap);
-                    }
-                }
+            char[,] filledScaledMap = (char[,])scaledMap.Clone();
+            // Fill in from outside
+            for (int x = 0; x < filledScaledMap.GetLength(0); x++) {
+                FloodFill(x, 0, filledScaledMap);
+                FloodFill(x, filledScaledMap.GetLength(1) - 1, filledScaledMap);
             }
-
-            // Fill in outside
-            for (int x = 0; x < filledMap.GetLength(0); x++) {
-                FloodFill(x, 0, filledMap, 'O', 'I');
-                FloodFill(x, filledMap.GetLength(1) - 1, filledMap, 'O', 'I');
+            for (int y = 0; y < filledScaledMap.GetLength(1); y++) {
+                FloodFill(0, y, filledScaledMap);
+                FloodFill(filledScaledMap.GetLength(0) - 1, y, filledScaledMap);
             }
-            for (int y = 0; y < filledMap.GetLength(1); y++) {
-                FloodFill(0, y, filledMap, 'O', 'I');
-                FloodFill(filledMap.GetLength(0) - 1, y, filledMap, 'O', 'I');
-            }
+            SaveBitmap(filledScaledMap);
 
-            PrintMap(filledMap);
-
-            int count = 0;
-            foreach (var c in filledMap) {
-                if (c == 'I') {
-                    count++;
-                }
-            }
-
-            return count.ToString();
+            return CountInternalSquares(filledScaledMap).ToString();
         }
  
-        void FloodFill(int x, int y, char[,] map, char fillChar = 'I', char emptyChar = ' ') 
+        void FloodFill(int x, int y, char[,] map, char fillChar = 'O', char emptyChar = ' ') 
         {
             Queue<(int, int)> toCheck = new();
             toCheck.Enqueue((x, y));
@@ -148,16 +116,16 @@ namespace AdventOfCode2023
         List<(int, int)> GetNeighbours(int x, int y, char[,] map, char emptyChar) 
         {
             List<(int, int)> results = new();
-            if (x > 0 && map [x - 1, y] == emptyChar) {
+            if (x - 1 >= 0 && map[x - 1, y] == emptyChar) {
                 results.Add((x - 1, y));
-            } 
-            if (x < map.GetLength(0) - 1 && map [x + 1, y] == emptyChar) {
+            }
+            if (x + 1 < map.GetLength(0) && map[x + 1, y] == emptyChar) {
                 results.Add((x + 1, y));
             }
-            if (y > 0 && map [x, y - 1] == emptyChar) {
+            if (y - 1 >= 0 && map[x, y - 1] == emptyChar) {
                 results.Add((x, y - 1));
-            } 
-            if (y < map.GetLength(1) - 1 && map [x, y + 1] == emptyChar) {
+            }
+            if (y + 1 < map.GetLength(1) && map[x, y + 1] == emptyChar) {
                 results.Add((x, y + 1));
             }
             return results;
@@ -170,6 +138,110 @@ namespace AdventOfCode2023
                 }
                 Console.WriteLine();
             }
+        }
+
+        void SaveBitmap(char[,] map) {
+            var bmp = new Bitmap(map.GetLength(0), map.GetLength(1));
+            for (int y = 0; y < map.GetLength(1); y++) {
+                for (int x = 0; x < map.GetLength(0); x++) {
+                    bmp.SetPixel(x, y, map[x, y] == ' ' ? Color.White : Color.Black);
+                }
+            }
+            bmp.Save("AdjustedMap.bmp", ImageFormat.Bmp);
+        }
+
+        void ReplaceStartSquare(int x, int y)
+        {
+            bool left = false;
+            bool up = false;
+            bool right = false;
+            bool down = false;
+            if (y - 1 > 0 && "7F|".Contains(puzzleInputLines[y - 1][x])) {
+                up = true;
+            }
+            if (y + 1 < puzzleInputLines.Length && "LJ|".Contains(puzzleInputLines[y + 1][x])) {
+                down = true;
+            }
+            if (x - 1 > 0 && "FL-".Contains(puzzleInputLines[y][x - 1])) {
+                left = true;
+            }
+            if (x + 1 < puzzleInputLines[0].Length && "J7-".Contains(puzzleInputLines[y][x + 1])) {
+                right = true;
+            }
+            if (left && right) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', '-');
+            } else if (left && up) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', 'J');
+            } else if (left && down) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', '7');
+            } else if (right && up) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', 'L');
+            } else if (right && down) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', 'F');
+            } else if (up && down) {
+                puzzleInputLines[y] = puzzleInputLines[y].Replace('S', '|');
+            }
+        }
+
+        void DrawPipe(int x, int y, char[,] map) {
+            var sX = x * 3;
+            var sY = y * 3;
+            switch (puzzleInputLines[y][x]) {
+                case '-': 
+                    map[sX, sY + 1] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX + 2, sY + 1] = '■';
+                    break;
+                case '|': 
+                    map[sX + 1, sY] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX + 1, sY + 2] = '■';
+                    break;
+                case 'J': 
+                    map[sX + 1, sY] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX, sY + 1] = '■';
+                    break;
+                case 'L': 
+                    map[sX + 1, sY] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX + 2, sY + 1] = '■';
+                    break;
+                case '7': 
+                    map[sX, sY + 1] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX + 1, sY + 2] = '■';
+                    break;
+                case 'F': 
+                    map[sX + 2, sY + 1] = '■';
+                    map[sX + 1, sY + 1] = '■';
+                    map[sX + 1, sY + 2] = '■';
+                    break;
+            }
+        }
+
+        int CountInternalSquares(char[,] scaledMap) 
+        {
+            int count = 0;
+            for (int x = 0; x < scaledMap.GetLength(0); x += 3) {
+                for (int y = 0; y < scaledMap.GetLength(1); y += 3) {
+                    if (SquareInternal(x, y, scaledMap)) {
+                        count ++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        bool SquareInternal(int x, int y, char[,] scaledMap, char spaceChar = ' ') {
+            for(int xOff = 0; xOff <= 2; xOff ++) {
+                for (int yOff = 0; yOff <= 2; yOff ++) {
+                    if (scaledMap[x + xOff, y + yOff] != spaceChar) {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
